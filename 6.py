3d699470d829ca4cf87e631b0bf923fbb3a0c59b
Karen <ka.spk27@gmail.com>
2023-01-03 16:43:49 +0300

    done

--- 6.py
+++ 6.py
@@ -1,4 +1,14 @@
 
+'''
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+# should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+'''
+
 def test(arr):
     if len(arr) == 0:
         return 0
@@ -26,30 +36,18 @@ def test(arr):
                     arr_new.append(summa)
         return max(arr_new)
 
-def max_sequence(arr):
-    maximum = 0
-    local_maximum = 0
+
+def max_seq(arr):
+    loc_max, max = 0, 0
     for i in arr:
-        if local_maximum > 0:
-            local_maximum += i
-            if local_maximum < 0:
-                local_maximum = 0
-            elif local_maximum > maximum:
-                maximum = local_maximum
+        if loc_max > 0:
+            loc_max += i
+            if loc_max > max:
+                max = loc_max
         elif i > 0:
-            local_maximum += i
-
-    return maximum
-
-def test_new(arr):
-    max, max_tmp = 0, 0
-    for i in arr:
-        if max_tmp > 0:
-            pass
-        elif max_tmp < 0:
-            pass
-
+            loc_max = i
     return max
+
 
 c = [7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43] #155
 x =[-2, -1, -3, -4, -1, -2, -1, -5, -4] #0
@@ -70,6 +68,6 @@ m_new = [-2, 1, -3, 4, -1, 2, 1, -7, 10] #6
 #print(f'pos - {pos}')
 #print(f'm - {mm}')
 
-my = test(m_new)
-nn = max_sequence(m_new)
+my = max_seq(m_new)
+nn = max_seq(m_new)
 print(f'my - {my}\nnn - {nn}')
