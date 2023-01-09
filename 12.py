581e8a4a568ba8800f818026e86993791227e756
Karen <ka.spk27@gmail.com>
2023-01-09 15:25:29 +0300

    to roman

--- 12.py
+++ 12.py
@@ -31,26 +31,73 @@ class RomanNumerals:
     def to_roman(val):
         val = str(val)
         res = ''
-        
+
         length = len(val)
         if length == 4:
             for i in range(int(val[0])):
                 res += 'M'
-            if int(val[1]) != 0 and int(val[1]) >= 1 and int(val[1]) <= 3:
+            val = val[1:]
+
+        if int(val[0]) != 0 and length >= 3:
+            if int(val[0]) >= 1 and int(val[0]) <= 3:
                 for d in range(int(val[1])):
                     res += 'C'
-        if length >= 3: 
-            pass
-        elif length == 2:
-            pass
-        elif length == 1:                
-            pass
+            elif int(val[0]) != 0 and int(val[0]) == 4:
+                res += 'CD'
+            elif int(val[0]) != 0 and int(val[0]) == 5:
+                res += 'D'
+            elif int(val[0]) >= 6 and int(val[0]) <= 8:
+                res += 'D'
+                for d in range(int(val[1]) - 5):
+                    res += 'C'
+            elif int(val[0]) >= 9:
+                res += 'CM'
+            val = val[1:]
+        elif int(val[0]) == 0 and length >= 3:
+            val = val[1:]
+
+        if int(val[0]) != 0 and length >= 2:
+            if int(val[0]) == 1:
+                res += 'X'
+            elif int(val[0]) >= 2 and int(val[0]) <= 3:
+                for i in range(int(val[0])):
+                    res += 'X'
+            elif int(val[0]) == 4:
+                res += 'XL'
+            elif int(val[0]) == 5:
+                res += 'L'
+            elif int(val[0]) >= 5 and int(val[0]) <= 8:
+                res += 'L'
+                for i in range(int(val[0]) - 5):
+                    res += 'X'
+            elif int(val[0]) >= 9:
+                res += 'XC'
+            val = val[1:]
+        elif int(val[0]) == 0 and length >= 2:
+            val = val[1:]
+
+        if int(val[0]) != 0 and length >= 1:
+            if int(val[0]) >= 1 and int(val[0]) <= 3:
+                for i in range(int(val[0])):
+                    res += 'I'
+            elif int(val[0]) == 4:
+                res += "IV"
+            elif int(val[0]) == 5:
+                res += 'V'
+            elif int(val[0]) >= 5 and int(val[0]) <= 8:
+                res += "V"
+                for i in range(int(val[0]) - 5):
+                    res += 'I'
+            elif int(val[0]) >= 9:
+                res += 'IX'
+
         return res
+            
 
     def from_roman(roman_num):
         return 0
 
 
-x = 2203
+x = 3999
 print(RomanNumerals.to_roman(x))
 print(RomanNumerals.from_roman(x))
